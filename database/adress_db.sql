383acb759ee0594ec9aa9847db432b54eeb0d392
DaniPinelli <elidaniypri@gmail.com>
2021-03-04 16:06:27 -0300

    Configuraciones iniciales y creacion de db

---
+++ database/adress_db.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE adress_db;
+USE adress_db;
+
+CREATE TABLE contact (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    adress VARCHAR(100) NOT NULL,
+    phone VARCHAR(15)
+);
+
+SHOW TABLES;
+
+DESCRIBE contact;
---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+
+const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+//Middelwares
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'adress_db'
+}, 'single'));
+
+
+//Routes
+
+app.listen(app.get('port'), () => console.log(`Server on port 3000`));
