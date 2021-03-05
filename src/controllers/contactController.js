575010d2edf0fb949a068213fe9568e8dca9fe7f
DaniPinelli <elidaniypri@gmail.com>
2021-03-05 12:07:56 -0300

    Metodo delete terminado

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -21,6 +21,20 @@ controller.save = (req, res) => {
             res.redirect('/');
         });
     })
-}
+};
+
+controller.delete = (req, res) => {
+    const {
+        id
+    } = req.params;
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM contact WHERE id = ?', [id], (err, rows) => {
+            res.redirect('/');
+        });
+    })
+
+};
+
+
 
 module.exports = controller;
--- src/routes/contact.js
+++ src/routes/contact.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const contactController = require('../controllers/contactController');
 
+//Read
 router.get('/', contactController.list);
 
+//Create
 router.post('/add', contactController.save);
+
+//Delete
+router.get('delete/:id', contactController.delete);
 
 module.exports = router;
